5f91a95ec502a3b2463ea8f68bb3c10449ff2b54
Guillaume <gplourde@proton.me>
2024-10-01 09:01:33 -0400

    Remove container build and cleanup build script

--- nvidia_build.sh
+++ nvidia_build.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-readonly BUILD_PATH="./build/"
-readonly SCRIPT_PATH="./scripts"
-readonly BASE_IMAGE="registry.fedoraproject.org/fedora"
+readonly BASE_IMAGE="quay.io/fedora/fedora-minimal"
 
-for FEDORA_MAJOR_VERSION in $(seq 40 40); do
-  podman build --build-arg=BASE_IMAGE=${BASE_IMAGE} --build-arg=FEDORA_MAJOR_VERSION=${FEDORA_MAJOR_VERSION} -t nvidia-driver-build -f Containerfile
-  podman run -it --rm --env FEDORA_MAJOR_VERSION=${FEDORA_MAJOR_VERSION} -v ./scripts/:/tmp/scripts/ -v ./rpms:/tmp/rpms:Z nvidia-driver-build /tmp/scripts/build.sh
-  #podman run -it --rm --env FEDORA_MAJOR_VERSION=${FEDORA_MAJOR_VERSION} -v ./scripts/:/tmp/scripts/ -v ./rpms:/tmp/rpms:Z nvidia-driver-build /tmp/scripts/test.sh
+for FEDORA_MAJOR_VERSION in $(seq 40 41); do
+  podman run -it --rm --env IS_LOCAL_BUILD=1 -v ./scripts/:/tmp/scripts/ -v ./rpms:/tmp/rpms:Z ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION} /tmp/scripts/build-nvidia-drv.sh
 done
-
-# publish to github
