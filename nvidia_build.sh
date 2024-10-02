fe8e9aa39d56eb2a38acee5330070c5f453d8d91
Guillaume <gplourde@proton.me>
2024-10-01 21:42:02 -0400

    update build script

--- nvidia_build.sh
+++ nvidia_build.sh
@@ -2,6 +2,6 @@
 
 readonly BASE_IMAGE="quay.io/fedora/fedora-minimal"
 
-for FEDORA_MAJOR_VERSION in $(seq 40 41); do
+for FEDORA_MAJOR_VERSION in $(seq 40 40); do
   podman run -it --rm -v ./scripts:/tmp/scripts:z -v ./rpms:/tmp/rpms:z ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION} /tmp/scripts/build-nvidia-drv.sh
 done
